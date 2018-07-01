20100b798b50d8f45485f07b8270ade88717d435
Efe Ariaroo <efeariaroo1@gmail.com>
2018-07-01 12:33:31 +0100

    Added json encoding of results from db

--- src/Main.purs
+++ src/Main.purs
@@ -1,30 +1,51 @@
 module Main where
 
 import Prelude
-import Effect (Effect)
-import Effect.Class (liftEffect)
-import Effect.Console (log, logShow)
-import Effect.Exception (error, Error)
 
 import Control.Monad.Error.Class (throwError)
 import Data.Bifunctor (lmap)
 import Data.Either (Either(..))
 import Data.Foldable (foldMap)
+import Data.Foldable (oneOf)
+import Data.Generic.Rep (class Generic)
+import Data.Generic.Rep.Show (genericShow)
+import Data.Map as M
 import Data.Maybe (Maybe(..))
 import Database.Postgres (Query(Query), connect, end, execute, execute_, query, queryOne_, queryValue_, query_, withClient, ClientConfig, ConnectionInfo, connectionInfoFromConfig, defaultPoolConfig, mkPool, release) as Pg
 import Database.Postgres.SqlValue (toSql)
 import Database.Postgres.Transaction (withTransaction)
-
+import Effect (Effect)
 import Effect.Aff (Aff, launchAff_)
-
+import Effect.Class (liftEffect)
+import Effect.Console (log, logShow)
+import Effect.Exception (error, Error)
 import Foreign (Foreign)
+import Foreign.Class (class Decode, class Encode)
+import Foreign.Generic (decodeJSON, encodeJSON, defaultOptions, genericDecode, genericEncode)
+import Foreign.Generic.Types (Options)
+import Models (User)
 import Node.Encoding (Encoding(..))
 import Node.HTTP (Request, Response, listen, createServer, setHeader, requestMethod, requestURL, responseAsStream, requestAsStream, setStatusCode)
 import Node.Stream (end, pipe, writeString)
 import Partial.Unsafe (unsafeCrashWith)
+import Routing (match)
+import Routing.Match (Match, bool, int, list, lit, nonempty, num, param, params, str)
 import Simple.JSON as JSON
 
-import Models (Users)
+
+data PostRoutes
+  = CustomerLogin String
+  | CustomerSignup String
+
+derive instance eqMyRoutes :: Eq PostRoutes
+derive instance genericMyRoutes :: Generic PostRoutes _
+instance showMyRoutes :: Show PostRoutes where show = genericShow
+
+routing :: Match PostRoutes
+routing = oneOf
+  [ CustomerLogin <$> (lit "api/v1/customerlogin" *> str)
+  , CustomerSignup <$> (lit "api/v1/customersignup" *> str)
+  ]
 
 
 main :: Effect Unit
@@ -37,9 +58,6 @@ main = do
     log "Listening on port 8080."
 
 
--- type User = { id :: Int, name :: String }
-
-
 router :: Request -> Response -> Effect Unit
 router req res = do
   setStatusCode res 200
@@ -48,27 +66,22 @@ router req res = do
       outputStream = responseAsStream res
   log (requestMethod req <> " " <> requestURL req)
 
+
+
   case requestMethod req of
     "GET" -> do
       pool <- Pg.mkPool connectionInfo
 
       launchAff_ $ Pg.withClient pool $ \conn -> do
-        users <- Pg.query_ read' (Pg.Query "select * from users order by first_name desc" :: Pg.Query Users) conn
+        users <- Pg.query_ read' (Pg.Query "select * from users order by first_name desc" :: Pg.Query User) conn
         
         liftEffect $ logShow users
 
+        liftEffect $ setHeader res "Content-Type" "text/plain"
+        _ <- liftEffect $ writeString outputStream UTF8 (encodeJSON users) (pure unit)
+        liftEffect $ end outputStream (pure unit)
       Pg.end pool
-      
-      let html = foldMap (_ <> "\n")
-            [ "<form method='POST' action='/'>"
-            , "  <input name='text' type='text'>"
-            , "  <input type='submit'>"
-            , "</form>"
-            ]
-      setHeader res "Content-Type" "text/html"
-      
-      _ <- writeString outputStream UTF8 html (pure unit)
-      end outputStream (pure unit)
+    
     "POST" -> void $ pipe inputStream outputStream
     _ -> unsafeCrashWith "Unexpected HTTP method"
 
--- src/Models.purs
+++ src/Models.purs
@@ -1,9 +1,18 @@
-module Models (Users) where
+module Models (User) where
 
+import Prelude
 import Data.Maybe
 
+import Data.Generic.Rep (class Generic)
+import Data.Generic.Rep.Show (genericShow)
 
-type Users = {
+import Foreign.Class (class Decode, class Encode)
+import Foreign.Generic (defaultOptions, genericDecode, genericEncode)
+import Foreign.Generic.Types (Options)
+
+import Simple.JSON as JSON
+
+newtype User = User {
   id :: Int, 
   user_uuid :: Maybe String,
   first_name :: Maybe String, 
@@ -16,3 +25,17 @@ type Users = {
   cloudinary_image_public_id :: Maybe String,
   is_enabled :: Maybe Boolean
 }
+
+derive instance genericUser :: Generic User _
+derive newtype instance foreignJsonUser :: JSON.ReadForeign User
+instance showForeignUser :: Show User
+  where show = genericShow
+
+instance decodeUser :: Decode User where
+  decode = genericDecode jsonOpts
+
+instance encodeUser :: Encode User where
+  encode = genericEncode jsonOpts
+
+jsonOpts :: Options
+jsonOpts = defaultOptions { unwrapSingleConstructors = true }
