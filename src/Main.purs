67e09bf55919b656a8515b9cfbb5ef16e2ea5d46
Efe Ariaroo <efeariaroo1@gmail.com>
2018-07-01 13:52:22 +0100

    Ramping up. Making progress.

--- src/Main.purs
+++ src/Main.purs
@@ -2,40 +2,34 @@ module Main where
 
 import Prelude
 
-import Control.Monad.Error.Class (throwError)
+import Control.Plus (empty)
 import Data.Bifunctor (lmap)
 import Data.Either (Either(..))
-import Data.Foldable (foldMap)
 import Data.Foldable (oneOf)
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
-import Data.Map as M
 import Data.Maybe (Maybe(..))
-import Database.Postgres (Query(Query), connect, end, execute, execute_, query, queryOne_, queryValue_, query_, withClient, ClientConfig, ConnectionInfo, connectionInfoFromConfig, defaultPoolConfig, mkPool, release) as Pg
-import Database.Postgres.SqlValue (toSql)
-import Database.Postgres.Transaction (withTransaction)
+import Database.Postgres (ClientConfig, ConnectionInfo, Query(Query), connectionInfoFromConfig, defaultPoolConfig, end, mkPool, query_, withClient) as Pg
 import Effect (Effect)
-import Effect.Aff (Aff, launchAff_)
+import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
-import Effect.Console (log, logShow)
+import Effect.Console (log)
 import Effect.Exception (error, Error)
 import Foreign (Foreign)
-import Foreign.Class (class Decode, class Encode)
-import Foreign.Generic (decodeJSON, encodeJSON, defaultOptions, genericDecode, genericEncode)
-import Foreign.Generic.Types (Options)
+import Foreign.Generic (encodeJSON)
 import Models (User)
 import Node.Encoding (Encoding(..))
 import Node.HTTP (Request, Response, listen, createServer, setHeader, requestMethod, requestURL, responseAsStream, requestAsStream, setStatusCode)
-import Node.Stream (end, pipe, writeString)
+import Node.Stream (Stream, Write, Writable, end, pipe, writeString)
 import Partial.Unsafe (unsafeCrashWith)
 import Routing (match)
-import Routing.Match (Match, bool, int, list, lit, nonempty, num, param, params, str)
+import Routing.Match (Match, lit, nonempty, str)
 import Simple.JSON as JSON
 
 
 data PostRoutes
-  = CustomerLogin String
-  | CustomerSignup String
+  = CustomerLogin Unit
+  | CustomerSignup Unit
 
 derive instance eqMyRoutes :: Eq PostRoutes
 derive instance genericMyRoutes :: Generic PostRoutes _
@@ -43,9 +37,25 @@ instance showMyRoutes :: Show PostRoutes where show = genericShow
 
 routing :: Match PostRoutes
 routing = oneOf
-  [ CustomerLogin <$> (lit "api/v1/customerlogin" *> str)
-  , CustomerSignup <$> (lit "api/v1/customersignup" *> str)
+  [ CustomerLogin <$> (lit "api/v1/customerlogin")
+  , CustomerSignup <$> (lit "api/v1/customersignup")
   ]
+
+
+handleCustomerLogin :: forall a. Request -> Response -> (Writable a) -> Effect Unit
+handleCustomerLogin req res outStream = do
+  pool <- Pg.mkPool connectionInfo
+
+  launchAff_ $ Pg.withClient pool $ \conn -> do
+    users <- Pg.query_ read' (Pg.Query "select * from users order by first_name desc" :: Pg.Query User) conn
+    
+    liftEffect $ log $ encodeJSON users
+
+    liftEffect $ setHeader res "Content-Type" "text/plain"
+    _ <- liftEffect $ writeString outStream UTF8 (encodeJSON users) (pure unit)
+    liftEffect $ end outStream (pure unit)
+  
+  Pg.end pool
 
 
 main :: Effect Unit
@@ -66,23 +76,21 @@ router req res = do
       outputStream = responseAsStream res
   log (requestMethod req <> " " <> requestURL req)
 
-
-
   case requestMethod req of
+    -- "GET" -> do
+    --   handleCustomerLogin req res outputStream
+    -- "POST" -> void $ pipe inputStream outputStream
+
     "GET" -> do
-      pool <- Pg.mkPool connectionInfo
+      handleCustomerLogin req res outputStream
+    "POST" -> 
+      -- handleCustomerLogin req res outputStream
+    -- log $ show $ (match routing (requestURL req))
 
-      launchAff_ $ Pg.withClient pool $ \conn -> do
-        users <- Pg.query_ read' (Pg.Query "select * from users order by first_name desc" :: Pg.Query User) conn
-        
-        liftEffect $ logShow users
-
-        liftEffect $ setHeader res "Content-Type" "text/plain"
-        _ <- liftEffect $ writeString outputStream UTF8 (encodeJSON users) (pure unit)
-        liftEffect $ end outputStream (pure unit)
-      Pg.end pool
-    
-    "POST" -> void $ pipe inputStream outputStream
+    case (match routing (requestURL req)) of 
+      Right (CustomerLogin _) -> 
+        handleCustomerLogin req res outputStream
+      _ -> void $ pipe inputStream outputStream
     _ -> unsafeCrashWith "Unexpected HTTP method"
 
 
